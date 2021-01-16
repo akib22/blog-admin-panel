835b96abfa0a741166719d08a4605695e02e794e
akib22 <adnanakib75@gmail.com>
2021-01-16 23:15:11 +0600

    Add user profile page

---
+++ src/components/BlogPost.js
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Typography, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import UserAvatar from './UserAvatar';
+
+const useStyles = makeStyles({
+  postTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  paper: {
+    padding: 16,
+  },
+});
+
+export default function BlogPost({ name, id }) {
+  const classes = useStyles();
+
+  return (
+    <Paper className={`mb-10 ${classes.paper}`}>
+      <UserAvatar data={{ name }} />
+      <Link to={`/posts/:${id}`} className={`link ${classes.postTitle}`}>
+        How to test your React app.
+      </Link>
+      <Typography className={classes.padding}>3 comments</Typography>
+    </Paper>
+  );
+}
+
+BlogPost.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+};
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,3 +1,106 @@
+import { Container, CardContent, Card, Avatar, Grid, Typography, Paper } from '@material-ui/core';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GithubIcon from '@material-ui/icons/GitHub';
+import { makeStyles } from '@material-ui/core/styles';
+import BlogPost from '../components/BlogPost';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  banner: {
+    background: 'cyan',
+    height: 150,
+    width: '100%',
+    zIndex: -1,
+  },
+  infoCard: {
+    marginTop: -40,
+    textAlign: 'center',
+    paddingTop: 40,
+  },
+  large: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    position: 'absolute',
+    top: 40,
+    left: '45%',
+  },
+  username: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  title: {
+    borderBottom: '1px solid #eee',
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  normalText: {
+    fontSize: 14,
+  },
+  text: {
+    fontWeight: '700',
+  },
+  card: {
+    padding: 15,
+  },
+}));
+
 export default function CreatePost() {
-  return <div>User</div>;
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className="mb-40">
+        <div className={classes.banner} />
+        <Container>
+          <Avatar className={classes.large} />
+          <Card className={classes.infoCard}>
+            <CardContent>
+              <Typography className={classes.username}>Jhon Willams</Typography>
+              <Typography>a@email.com</Typography>
+              <div className="mt-8 flex justify-center align-center">
+                <GithubIcon />
+                <TwitterIcon style={{ margin: '0 10px' }} />
+                <FacebookIcon />
+              </div>
+            </CardContent>
+          </Card>
+        </Container>
+      </div>
+      <Container>
+        {/* Info about user */}
+        <Grid container spacing={3}>
+          <Grid item xs={4} sm={4}>
+            <Paper className={`mb-10 ${classes.card}`}>
+              <Typography className={classes.title}>Skills</Typography>
+              <Typography className={classes.normalText}>
+                PHP, Java, Python, SQL, MySQL, HTML, CSS, JS, WordPress, Symfony, Silex, Jenkins,
+                Git, Ubuntu, Shell, Scripting, Interfaces, Azure, SSO, ETL
+              </Typography>
+            </Paper>
+            <Paper className={classes.card}>
+              <Typography className={classes.text}>3 posts</Typography>
+              <Typography className={classes.text}>5 comments</Typography>
+            </Paper>
+          </Grid>
+
+          {/* Post List */}
+          <Grid item xs={10} sm={8}>
+            <BlogPost className={classes.paper} name="Jhon Willams" id={1} />
+            <BlogPost className={classes.paper} name="Jhon Willams" id={1} />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
